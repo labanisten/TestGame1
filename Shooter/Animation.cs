6596e1dc24e6f290f6b7bd81bedb474fe8b8696d
labanisten <svedhaug@hotmail.com>
2013-08-17 16:58:13 +0200

    new file:   Shooter/Animation.cs

---
+++ Shooter/Animation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Shooter
+{
+    class Animation
+    {
+
+        Texture2D spriteStrip;
+
+        float scale;
+
+        int elapsedTime;
+
+        int frameTime;
+
+        int frameCount;
+
+        int currentFrame;
+
+        Color color;
+
+
+    }
+}
